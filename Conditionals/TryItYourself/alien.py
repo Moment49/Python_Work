1fcb21e8bcf1791a7c757dbdb9b407bc987ab011
ibenacho Elvis <ibenachoelvis49@gmail.com>
2023-03-14 15:17:40 +0100

    updated

--- Conditionals/TryItYourself/alien.py
+++ Conditionals/TryItYourself/alien.py
@@ -71,3 +71,22 @@
 #     print("You really like oranges")
 
 # 5.8
+# usernames = ['moment47', 'admin', 'vermic49', 'babs12', 'elv35']
+usernames = ['Vermic49']
+
+for username in usernames:
+    if username == 'admin':
+        print(f"Hello {username}, would you like to see a status report?")
+    else:
+        print(f"Hello {username},thank you for logging in again")
+
+# 5.9
+if usernames:
+    for username in usernames:
+        print(f"Hello {username}, how are you doing?")
+else:
+    print("\nWe need to find some users!")
+
+# 5.10
+
+    
